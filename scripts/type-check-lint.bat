bc7248b8d909a9a348c4c03ad4f06d4de2fb0f11
DimitrisAfendras <diafentras@groupnet.gr>
2025-07-29 20:43:38 +0300

    Remove scripts folder and update lint-staged configuration

--- scripts/type-check-lint.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-REM This script ignores any arguments passed to it and just runs TypeScript with our custom configuration
-npx tsc --noEmit --skipLibCheck -p tsconfig.lint.json
--- scripts/type-check-lint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# This script ignores any arguments passed to it and just runs TypeScript with our custom configuration
-tsc --noEmit --skipLibCheck -p tsconfig.lint.json
